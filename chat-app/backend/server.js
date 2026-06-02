393f9b764a4baa8319ac5f9629e75ed13a7d29c8
iswat <ibeetek@gmail.com>
2026-06-03 00:53:30 +0100

    chore: remove unused `connection` import from websocket module

--- chat-app/backend/server.js
+++ chat-app/backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { connection, server as WebSocketServer } from "websocket";
+import { server as WebSocketServer } from "websocket";
 import http from "http";
 
 const app = express();
